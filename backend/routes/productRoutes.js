e2d845b7aa5ab9fc477dfc947c2b3d9041c1e067
Canberk <a-xy.cnbrk@hotmail.com>
2024-02-14 06:52:08 +0300

    Get all products, create new products and get product with id implemented. Don't use POST.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,2 +1,16 @@
 const express = require('express');
-//const productController = require('./../controllers/productController');
+const productController = require('./../controllers/productController');
+
+const router = express.Router();
+
+router
+    .route('/')
+    // Get all products
+    .get(productController.getAllProducts)
+    // Not Finished Don't Use.
+    .post(productController.createProduct);
+
+router
+    .route('/:id')
+    // Get spesific data (Parameter = id)
+    .get(productController.getProduct);
