6ab87fee012784551751bbe81cae5749e6342ecd
Canberk <a-xy.cnbrk@hotmail.com>
2024-02-15 16:18:21 +0300

    Password Bcrypt added.

--- backend/models/staffModel.js
+++ backend/models/staffModel.js
@@ -1,5 +1,7 @@
+const crypto = require('cryptojs');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const staffSchema = new mongoose.Schema({
     email: {
@@ -53,6 +55,45 @@ const staffSchema = new mongoose.Schema({
     passwordResetToken: String,
     passwordResetExpires: Date,
 });
+
+staffSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+staffSchema.pre('save', function(next){
+    if(!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+staffSchema.methods.passwordConfirmation = async function(
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+staffSchema.methods.changedPasswordAfter = function(JWTTimestamp){
+    if(this.passwordChangedAt){
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10
+        );
+
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    // NOT Changed
+    return false;
+}
+
 
 const Staff = mongoose.model('Staff', staffSchema);
 
