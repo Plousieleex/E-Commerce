52a4a4f7a6e849f3cea1678897f848023f4af6a7
Canberk <a-xy.cnbrk@hotmail.com>
2024-02-24 19:55:19 +0300

    Product routes designed.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const productController = require('./../controllers/productController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router
+    .route('/')
+    // Only admins and staffs can create products
+    .post(
+        authController.protect,
+        authController.permit('admin', 'staff'),
+        productController.createProduct)
+    .get(
+        authController.protect,
+        authController.permit('admin', 'staff'),
+        productController.getProducts);
+
+router
+    .route('/:id')
+    .get(
+        productController.getProductDetailed
+    );
+
+router
+    .route('/showcase/:id')
+    .get(
+        productController.getProductShowcase
+    );
+
+
+
+module.exports = router;
