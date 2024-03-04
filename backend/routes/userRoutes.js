630be7da7eaa803f33ccefb5af806eb7cbf7d4b2
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-04 22:36:34 +0300

    Address routes implemented to User Routes.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const authController = require('./../controllers/authController');
 const userController = require('./../controllers/userController');
+const addressRouter = require('./../routes/addressRoutes');
 
 const router = express.Router();
+
+router.use('/:userId/address', addressRouter);
 
 router
     .route('/signup')
