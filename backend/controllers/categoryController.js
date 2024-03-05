2905e901174c8d346488cbbd4ec62bfae23d48db
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-05 19:28:01 +0300

    Product title controllers added..

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,30 +1,40 @@
 const subCategory = require('../models/subCategoryModel');
 const parentCategory = require('../models/parentCategoryModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+const productTitle = require('./../models/productTitle');
 
 // Create Sub Category
 exports.createSubCategory = factory.createOne(subCategory);
 // Create Parent Category
 exports.createParentCategory = factory.createOne(parentCategory);
+// Create Product Title
+exports.createProductTitle = factory.createOne(productTitle);
 
 // Update Sub Category
 exports.updateSubCategory = factory.updateOne(subCategory);
 // Update Parent Category
 exports.updateParentCategory = factory.updateOne(parentCategory);
+// Create Product Title
+exports.updateProductTitle = factory.updateOne(productTitle);
 
 // Get Sub Category (One Doc)
 exports.getSubCategory = factory.getOne(subCategory);
 // Get Parent Category (One Doc)
 exports.getParentCategory = factory.getOne(parentCategory);
+// Get Product Title (One Doc)
+exports.getProductTitle = factory.getOne(productTitle);
 
 // Get Sub Category (All)
 exports.getAllSubCategory = factory.getAll(subCategory);
 // Get Parent Category (All)
 exports.getAllParentCategory = factory.getAll(parentCategory);
+// Get Product Title (All)
+exports.getAllProductTitle = factory.getAll(productTitle);
 
 // Delete Sub Category
 exports.deleteSubCategory = factory.deleteOne(subCategory);
 // Delete Parent Category
 exports.deleteParentCategory = factory.deleteOne(parentCategory);
+// Delete Product Title
+exports.deleteProductTitle = factory.deleteOne(productTitle);
+
