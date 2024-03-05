70143d6f7389e8a533320de1f6165d109482fc88
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-05 19:21:06 +0300

    Review ratings implemented.

--- backend/models/reviewModel.js
+++ backend/models/reviewModel.js
@@ -31,6 +31,48 @@ const reviewSchema = new mongoose.Schema({
   toObject: {virtuals: true}
 });
 
+reviewSchema.index({ tour: 1, user: 1}, { unique: true });
+
+reviewSchema.statics.calcAverageRatings = async function(productId) {
+  const stats = await this.aggregate([
+    {
+      $match: {product: productId}
+    },
+    {
+      $group: {
+        _id: '$product',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  if (stats.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsQuantity: stats[0].nRating,
+      productRating: stats[0].avgRating
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsQuantity: 0,
+      productRating: 4.5 
+    });
+  }
+};
+
+reviewSchema.post('save', function(){
+  this.constructor.calcAverageRatings(this.product);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next){
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  await this.r.constructor.calcAverageRatings(this.r.product);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
