e9c7938eec08d806eac742627d874f523b281ff6
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-14 04:24:48 +0300

    Product Category model added for Products.

---
+++ backend/models/productCategoryModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const productCategorySchema = new mongoose.Schema({
+    subCategory: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'SubCategory',
+        required: true
+    },
+    productCategoryTitle: {
+        type: String,
+        unique: [true, 'You have created a product category with this title.'],
+        required: [true, 'A product category must have a title.']
+    },
+    productCategoryDescription: {
+        type: String
+    },
+    productCategoryImage: {
+        type: [String]
+    }
+});
+
+productCategorySchema.pre('save', async function(next){
+    const subCategoryToUpdate = await mongoose.model('SubCategory').findByIdAndUpdate(
+        this.subCategory,
+        {$addToSet: {productCategories: this._id}}
+    );
+    next();
+});
+
+productCategorySchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'subCategory',
+        select: 'subCategoryTitle'
+    });
+    next();
+});
+
+const ProductCategory = mongoose.model('ProductCategory', productCategorySchema);
+
+module.exports = ProductCategory;
