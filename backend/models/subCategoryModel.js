bfaf96ba3bd2ecdc73e7be87801e9fbf8e302b0a
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-14 04:25:14 +0300

    Sub Category model revised.

--- backend/models/subCategoryModel.js
+++ backend/models/subCategoryModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const subCategorySchema = new mongoose.Schema({
+    parentCategory: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'ParentCategory',
+        required: true
+    },
     subCategoryTitle: {
         type: String,
         unique: [true, 'You have created a sub-category with that title.'],
@@ -11,7 +16,29 @@ const subCategorySchema = new mongoose.Schema({
     },
     categoryImage: {
         type: [String]
-    }
+    },
+    productCategories: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'ProductCategory'
+        }
+    ]
+});
+
+subCategorySchema.pre('save', async function(next){
+    const parentCategoryToUpdate = await mongoose.model('ParentCategory').findByIdAndUpdate(
+        this.parentCategory,
+        {$addToSet: {subCategories: this._id}}
+    );
+    next();
+});
+
+subCategorySchema.pre(/^find/, function (next){
+    this.populate({
+        path: 'parentCategory',
+        select: 'parentCategoryTitle'
+    });
+    next();
 });
 
 const SubCategory = mongoose.model('SubCategory', subCategorySchema);
