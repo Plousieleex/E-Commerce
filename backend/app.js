0740c31a75f3e259782b5d67c1379817e575da19
Canberk <a-xy.cnbrk@hotmail.com>
2024-03-20 19:55:26 +0300

    App.js implements

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,63 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
 const globalErrorHandler = require('./controllers/errorController');
 
 const productRouter = require('./routes/productRoutes');
 const categoryRouter = require('./routes/categoryRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require("./utils/appError");
 
 const app = express();
+
+app.enable('trust proxy');
+
+// GLOBAL MIDDLEWARES
+app.use(cors());
+
+app.options('*', cors());
+
+app.use(helmet());
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+   max: 100,
+   windowMs: 60 * 60 * 1000,
+   message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb'}));
+app.use(express.urlencoded({ extended: true, limit: '10kb'}));
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(compression());
 
 
 // Routes
 app.use('/api/v1/category', categoryRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use(globalErrorHandler);
 
